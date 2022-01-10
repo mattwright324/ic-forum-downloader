2b368c2d0a3bb464504fe1868c31c1e3446e03ae
Matthew Wright <mattwright324@gmail.com>
2022-01-10 10:26:19 -0500

    working script

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ic-forum-downloader
+
 Script to download Invision Community forum attachments.
+
+## How it works
+
+Most Invision Community forum sites have attachment files by appending path to the end `/applications/core/interface/file/attachment.php?id=` and incrementing the number on the end.
+
+However, most IC forums also have a setting that prevents this from working in most cases. There are some forums that it does work on.
+
+## Usage
+
+Update the script variables `forumPattern`, `start`, and `end` then run the script in your preferred way, VS Code works well.
+
+The script will download files from `start` to `end` in a relative folder to the script in the name of the forum domain name.
+
+How do you know when you've reached the last attachment? It can be hard to tell but there will be nothing but errors and no more files downloaded in the console output.
---
+++ downloader.ps1
@@ -0,0 +1,51 @@
+﻿# Set path to current script location
+$path = $MyInvocation.MyCommand.Path
+if (!$path) {$path = $psISE.CurrentFile.Fullpath}
+if ($path)  {$path = Split-Path $path -Parent}
+Set-Location $path
+
+# Settings
+$forumPattern = "https://forums.getpaint.net/applications/core/interface/file/attachment.php?id="
+$folder = -join("/", ([uri]$forumPattern).Authority, "/");
+$outputLocation = -join($path, $folder)
+$begin = 1
+$end = 100
+
+mkdir -p $outputLocation -Force
+
+Write-Host $forumPattern
+Write-Host $outputLocation
+
+for ($i = $begin; $i -le $end; $i++) {
+    $url = -join($forumPattern, $i)
+    
+    Write-Host
+    try {
+        $download = Invoke-WebRequest -Uri $url
+
+        $content = [System.Net.Mime.ContentDisposition]::new($download.Headers["Content-Disposition"])
+        Write-Host $download.Headers
+        if ($content.ToString() -like "*=UTF-8''*") {
+            $fileName = ($content.ToString() -split "UTF\-8''")[1]
+        } else {
+            $fileName = $content.FileName
+        }
+
+        # Files without extension of type unknown are likely zip files
+        if ($fileName -notlike  '\.' -And $download.Headers["Content-Type"] -like "*application/x-unknown*") {
+            $fileName = -join($fileName, '.zip');
+        }
+        
+        $filePath = -join($outputLocation, $fileName)
+        $file = [System.IO.FileStream]::new($filePath, [System.IO.FileMode]::Create)
+        $file.Write($download.Content, 0, $download.RawContentLength)
+        $file.Close()
+
+        Write-Host $url
+        $debug = -join($fileName, ", size: ", $download.RawContentLength)
+        Write-Host $debug
+    } catch {
+        Write-Host $url -ForegroundColor red
+    }
+}
+
